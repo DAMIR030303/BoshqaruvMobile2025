ad3ead09e804e83f2fcac32a52bd68e4d8b76584
Developer <developer@example.com>
2025-08-28 13:41:45 +0500

    Fix plugin errors by removing problematic dependencies and simplifying configuration

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      // React Native uchun kerakli pluginlar
-      'react-native-reanimated/plugin',
-      [
-        'module-resolver',
-        {
-          root: ['./src'],
-          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-          alias: {
-            '@': './src',
-            '@components': './src/components',
-            '@screens': './src/screens',
-            '@navigation': './src/navigation',
-            '@services': './src/services',
-            '@store': './src/store',
-            '@utils': './src/utils',
-            '@assets': './assets',
-          },
-        },
-      ],
-    ],
+    presets: ['babel-preset-expo']
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -2,18 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// React Native uchun qo'shimcha sozlamalar
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
-
-// Asset fayllarini to'g'ri ishlash uchun
-config.resolver.assetExts.push('db', 'mp3', 'ttf', 'obj', 'png', 'jpg', 'jpeg', 'gif');
-
-// Source map uchun
-config.transformer.minifierConfig = {
-  keep_fnames: true,
-  mangle: {
-    keep_fnames: true,
-  },
-};
-
 module.exports = config;
